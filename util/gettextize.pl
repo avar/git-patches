c0cd01f97596b760816ed3253e4ce375863639da
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-09-01 18:39:04 +0000

    a utility to gettextize git

---
+++ util/gettextize.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+use 5.12.0;
+use autodie;
+
+my $f = shift;
+
+open my $in, "<", $f;
+open my $out, ">", "$f.tmp";
+
+my $cont = join '', <$in>;
+
+$cont =~ s[
+    \b
+    (?<func>(?:die|die_errno|warning|fprintf|printf))
+    (?<w>\ *)
+    \( (?<cont>.*?) \);
+][
+munge($+{func}, $+{w}, $+{cont});
+]exg;
+
+sub munge {
+    my ($func, $w, $cont) = @_;
+
+    $cont =~ s["(.*)"][_("$1")];
+
+    return "$func$w($cont);";
+}
+
+print $out $cont;
+close $_ for $in, $out;
+rename "$f.tmp", $f;
