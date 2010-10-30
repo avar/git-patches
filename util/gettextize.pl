ba18b7a490fc2eb058d50ecc6159ad7081cacfb5
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-10-30 19:43:50 +0000

    util/gettextize.pl: skip more stuff & fix the logic to not double-gettext things
    Now I can just run:
    
    $ git reset --hard && for i in $(git diff --stat master.. | ack '\s(.*?\.c)' --output '$1' | grep -v -e fast-import -e imap-send -e shell -e show-index -e upload-pack -e merge-recursive -e merge-index -e merge-tree -e fetch-pack -e mktag -e pack-redundant -e hash-object -e index-pack -e envsubst -e receive-pack -e send-pack -e http-push -e http-fetch -e http-backend -e daemon -e unpack-file -e var.c -e git.c -e remote); do echo $i && perl /home/avar/g/git-patches/util/gettextize.pl $i; done && git diff -U0
    HEAD is now at 9deebba WIP: git-sh-i18n--envsubst for a correct eval_gettext
    builtin/add.c
    builtin/archive.c
    builtin/branch.c
    builtin/bundle.c
    builtin/checkout.c
    builtin/clean.c
    builtin/clone.c
    builtin/commit.c
    builtin/describe.c
    builtin/diff.c
    builtin/fetch.c
    builtin/gc.c
    builtin/grep.c
    builtin/init-db.c
    builtin/log.c
    builtin/merge.c
    builtin/mv.c
    builtin/notes.c
    builtin/pack-refs.c
    builtin/patch-id.c
    builtin/push.c
    builtin/reset.c
    builtin/revert.c
    builtin/rm.c
    builtin/shortlog.c
    builtin/tag.c
    gettext.c
    t/t0200/test.c
    wt-status.c

--- util/gettextize.pl
+++ util/gettextize.pl
@@ -21,11 +21,61 @@ munge($+{func}, $+{w}, $+{cont});
 sub munge {
     my ($func, $w, $cont) = @_;
 
-    $cont =~ s[(?<!_\()"(.*)"][
-        $1 ~~ [ "%s\\n" ]
-        ? qq["$1"]
-        : qq[_("$1")]
-    ]es;
+    if ($cont =~ /\s*_\(/s) {
+        #warn "Skipping <$cont>";
+        goto end;
+    }
+
+    if ($cont =~ /\Q"%s %s... %s\n"/ or
+        $cont =~ /\Q"# %s\n"/ or
+        $cont =~ /\Q"#\n"/ or
+        $cont =~ /\Q"%s"/ or
+        $cont =~ /\Q"\n"/ or
+        $cont =~ /\Q"-%d-g%s"/ or
+        $cont =~ /\Q" %-11s %8d %s\n"/ or
+        $cont =~ /\Q"%s %s\n"/ or
+        $cont =~ /\Q"%s\t%s\t%s"/ or
+        $cont =~ /\Q" %s\n"/ or
+        $cont =~ /\Q"%s%s\n"/ or
+        $cont =~ /\Q"%.*s"/ or
+        $cont =~ /\Q" %s"/ or
+        $cont =~ /\Q"\t%s\n"/ or
+        $cont =~ /\Q" * [%s] %s\n"/ or
+        $cont =~ /\Q"%s\t%s\n"/ or
+        $cont =~ /\Q" %.*s\n"/ or
+        $cont =~ /\Q"%-15s "/ or
+        $cont =~ /\Q"\n    "/ or
+        $cont =~ /\Q"%s=%s\n"/ or
+        $cont =~ /\Q"[%"PRIuMAX"] "/
+        or $cont =~ /\Q" %s%c"/
+        or $cont =~ /\Q"%s%c"/
+        or $cont =~ /\Q"%s -> "/
+        or $cont =~ /\Q"%s %s%c"/
+        or $cont =~ /\Q"%stag %s%s\n"/
+        or $cont =~ /\Q"%stree %s%s\n\n"/
+        or $cont =~ /\Q"-- \n%s\n\n"/
+        or $cont =~ /\Q"\n--%s%s--\n\n\n"/
+        or $cont =~ /\Q"%c %s %s\n"/
+        or $cont =~ /\Q"%c %s\n"/
+        or $cont =~ /\Q".\n"/
+        or $cont =~ /\Q"'%s': %s"/
+        or $cont =~ /\Q"rm '%s'\n"/
+        or $cont =~ /\Q"git rm: '%s'"/
+        or $cont =~ /\Q"%6d\t%s\n"/
+        or $cont =~ /\Q"%s (%d):\n"/
+        or $cont =~ /\Q"      %s\n"/
+    ) {
+        #warn "Skipping pure format <$cont>";
+        goto end;
+    }
+
+  munge:
+        $cont =~ s[(?<!_\()"(.*)"][
+            $1 ~~ [ "%s\\n" ]
+            ? qq["$1"]
+            : qq[_("$1")]
+        ]es;
+  end:
 
     return "$func$w($cont);";
 }
